3f6e6cfbdb4e995a36cefd725d07a0ebd1125c39
DoVale-Daniel <dovale.daniel07@gmail.com>
2018-03-17 15:27:39 -0300

    Criação do método da API responsável por fazer a integração entre os sistemas de Rastreamento e Vendas, fazendo a tradução do request para um response adequado ao sistema de vendas

--- IntegracaoVendas.API/IntegracaoVendas.API/Controllers/IntegrationController.cs
+++ IntegracaoVendas.API/IntegracaoVendas.API/Controllers/IntegrationController.cs
@@ -1,9 +1,10 @@
-﻿using IntegracaoVendas.API.Util;
+﻿using IntegracaoVendas.API.Extensions;
+using IntegracaoVendas.API.Util;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
-using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Web.Http;
-using IntegracaoVendas.API.Extensions;
 
 namespace IntegracaoVendas.API.Controllers
 {
@@ -18,22 +19,65 @@ namespace IntegracaoVendas.API.Controllers
         /// <summary>
         /// Método responsável por fazer a comunicação entre as plataformas de Rastreamento e Vendas, realizando a integração entre os dados
         /// </summary>
-        /// <param name="postObject">Contém os dados que deverão ser integrados e processados</param>
-        /// <returns></returns>
+        /// <param name="postObject">Contém os dados no formato do sistema de Rastreamento, que deverão ser integrados e processados</param>
+        /// <returns>Novo objeto no formato esperado pelo sistema de Vendas</returns>
         [HttpPost]
-        [HttpGet]
         [Route("IntegrateTrackingSalesSystem")]
-        public async Task<IHttpActionResult> IntegrateTrackingSalesSystem(int postObject)
+        public async Task<IHttpActionResult> IntegrateTrackingSalesSystem(string orderData)
         {
             try
             {
-                //Retorna para o request sucesso na operação
-                return await Task.FromResult(Ok());
+                //Deserializa o json recebido em um objeto genérico, com o objetivo de criar um novo objeto apenas com as propriedades relevantes
+                JObject jsonData = JObject.Parse(orderData);
+
+                //Cria um novo objeto no formato suportado pelo sistema de Vendas
+                var salesObject = new
+                {
+                    //Recupera o id do pedido
+                    orderId = jsonData["order_id"],
+                    //Recupera a descrição do enum já traduzida entre os status dos dois sistemas
+                    status = this.GetEnumDescriptionByValue<EnumStatus.OrderStatus>(jsonData["event"]["status_id"].ToString()),
+                    //Recupera a data do pedido
+                    date = jsonData["event"]["date"]
+                };
+
+                //Serializa o novo objeto já convertido para o formato adequado ao sistema de vendas
+                string jsonResult = JsonConvert.SerializeObject(salesObject);
+
+                //Retorna para o request o objeto traduzido
+                return await Task.FromResult(Ok(jsonResult));
             }
             catch(Exception)
             {
                 //Retorna para o request erro no processamento
                 return base.InternalServerError();
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Método genérico responsável por retornar a descrição do enum a partir de seu valor
+        /// </summary>
+        /// <typeparam name="TEnum">Tipo do enum que será manipulado</typeparam>
+        /// <param name="value">Valor do enum especificado</param>
+        /// <returns>Descrição do Enum especificado</returns>
+        private string GetEnumDescriptionByValue<TEnum>(string value)
+            where TEnum : struct
+        {
+            try
+            {
+                //Recupera o enum genérico a partir do seu tipo e valor
+                Enum enu = (Enum)Enum.Parse(typeof(TEnum), value);
+
+                //Retorna a descrição do enum especificado
+                return enu.GetEnumDescription();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
             }
         }
 
